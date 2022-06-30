3c91178e2b175ae2d61838e95dd9bc33cdc08a0c
Hafizh Caesandro Kevin <hake.protest@gmail.com>
2022-06-30 13:39:41 +0700

    Initial Commit

---
+++ MinProPokemon/MinProPokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinProPokemonApp.swift
+//  MinProPokemon
+//
+//  Created by Hafizh Caesandro Kevinoza on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MinProPokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
